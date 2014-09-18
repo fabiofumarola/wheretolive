52c520129f11bc5f7948f4b1993a5434c290da09
fabiofumarola <fabiofumarola@gmail.com>
2014-09-18 14:55:08 +0200

    inital template

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wheretoliveApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wheretoliveApp
+ */
+angular.module('wheretoliveApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wheretoliveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wheretoliveApp
+ */
+angular.module('wheretoliveApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
