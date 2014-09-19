ff78d43439389089bddc8fe0c9284d6460e3320e
fabiofumarola <fabiofumarola@gmail.com>
2014-09-19 15:53:22 +0200

    add missing files

---
+++ app/scripts/controllers/news.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wheretoliveApp.controller:NewsCtrl
+ * @description
+ * # NewsCtrl
+ * Controller of the wheretoliveApp
+ */
+angular.module('wheretoliveApp')
+  .controller('NewsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.map = {
+        center: {
+            latitude: '41',
+            longitude: '16'
+        },
+        zoom: 8
+    };
+  });
