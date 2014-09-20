259f68984e2d490a67af7a942d8755836fe5ee84
Fabiana Lanotte <fabiana.lan@hotmail.it>
2014-09-20 18:53:06 +0200

    prima versione di marker che evidenziano news

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -10,6 +10,8 @@
 angular.module('wheretoliveApp')
     .controller('NewsCtrl', ['$scope', 'Search', function ($scope, Search) {
 
+        var lastMarkerIdHighlight = "";
+
         $scope.paginationCurrentPage = 0;
         var paginationPageSize = 15;
         var paginationPageCount = Math.ceil($scope.results / paginationPageSize) - 1;
@@ -41,23 +43,22 @@ angular.module('wheretoliveApp')
             $scope.paginationCurrentPage = newPage;
         };
 
-        $scope.disablePrevPage = function(){
-            return $scope.paginationCurrentPage === 0 ;
+        $scope.disablePrevPage = function () {
+            return $scope.paginationCurrentPage === 0;
         };
 
-        $scope.disableNextPage = function(){
-            var res = ($scope.paginationCurrentPage === paginationPageCount) ||( paginationPageCount=== -1);
+        $scope.disableNextPage = function () {
+            var res = ($scope.paginationCurrentPage === paginationPageCount) || ( paginationPageCount === -1);
             //console.log("Disable: "+ res);
             return res;
         };
 
 
-        $scope.updateSearch = function(newPage){
+        $scope.updateSearch = function (newPage) {
             paginationSetCurrentPage(newPage);
             $scope.getLatestNews();
-            $('html,body, div.scrollit').animate({scrollTop:0},'slow')
+            $('html,body, div.scrollit').animate({scrollTop: 0}, 'slow')
         };
-
 
 
         /*
@@ -69,6 +70,31 @@ angular.module('wheretoliveApp')
                 longitude: '16'
             },
             zoom: 8
+        };
+
+        $scope.markersEvents = {
+            click: function (gMarker, eventName, model) {
+                if (model.$id) {
+                    //model = model.coords;//use scope portion then
+                }
+                console.log("Sto cliccando " + model.id + "--" + model.title);
+                //alert("Model: event:" + eventName + "gMarker: " +gMarker+" "+ JSON.stringify(model));
+                var idNews = model.id.split("/")[0];
+                console.log(idNews);
+
+
+
+                $("#" + lastMarkerIdHighlight).removeClass("highlightPost");
+                $("#" + idNews).addClass("highlightPost");
+                console.log(lastMarkerIdHighlight);
+
+                // $('html,body, div.scrollit').animate({scrollTop:0},'slow')
+
+                $('html,body, div.scrollit').animate({scrollTop: $("#" + idNews).offset().top - 150}, 'slow');
+
+                lastMarkerIdHighlight = idNews;
+
+            }
         };
 
 
@@ -96,7 +122,7 @@ angular.module('wheretoliveApp')
         $scope.getLatestNews = function () {
             var from = paginationPageSize * $scope.paginationCurrentPage;
             //var from = 0;
-            console.log("GetLatestNews FROM: "+from);
+            console.log("GetLatestNews FROM: " + from);
             Search.getLastNews($scope.querySize, from).then(function (data) {
                 $scope.newsArray = data.data.hits.hits;
                 $scope.results = data.data.hits.total;
@@ -129,20 +155,24 @@ angular.module('wheretoliveApp')
 
             //creo l'array dei markers a partire dal json
             var markers = new Array();
-
+            var func = function () {
+                console.log('mammamé');
+            };
             var count = 0;
             for (var i = 0; i < jsonData.length; i++) {
 
                 for (var p = 0; p < jsonData[i]._source.positions.length; p++) {
                     // console.log("mi sa che qui non entro");
                     var newMarker = {
-                        id: parseInt(count),
+                        id: jsonData[i]._id + "/" + count,
                         latitude: parseFloat(jsonData[i]._source.positions[p].lat),
                         longitude: parseFloat(jsonData[i]._source.positions[p].lon),
                         showWindow: true,
-                        title: 'Marker' + i
+                        title: jsonData[i]._source.title
+
                     };
-                    // console.log(newMarker.latitude + '--' + newMarker.longitude);
+
+                    //console.log(newMarker.latitude + '--' + newMarker.longitude);
                     markers.push(newMarker);
                     count++;
                 }
