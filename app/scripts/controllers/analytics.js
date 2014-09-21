0821c898352d1c075a42e8829d18658ee2a07315
Giuseppe Piccolo <giuseppe.piccolo1@gmail.com>
2014-09-21 14:15:43 +0200

    Merge

--- app/scripts/controllers/analytics.js
+++ app/scripts/controllers/analytics.js
@@ -187,39 +187,40 @@ angular.module('wheretoliveApp')
 
             $scope.aggregateTotalCrimesInCity = function (city) {
             var res = Search.aggregateTotalCrimesInCity(city).then(function (data) {
-                console.log(data);
-                $scope.newsArray = data.data.aggregations.crimes_count.buckets;
-                console.log('Array', $scope.newsArray);
-                // setMarkersNews($scope.newsArray);
+                var res1 = data.data.aggregations.crimes_count.buckets;
+                console.log("aggregateTotalCrimesInCity res", res1);
+                return res1;
 
 
             });
+            return res;
         };
 
         $scope.aggregateTotalNewsInCity = function (city) {
             var res = Search.aggregateTotalNewsInCity(city).then(function (data) {
-                console.log(data);
-                $scope.newsArray = data.data.aggregations.crimes_count.buckets;
-                console.log('Array', $scope.newsArray);
+                var res1 = data.hits.total;
+                console.log("aggregateTotalNewsInCity res", res1);
+                return res1;
                 // setMarkersNews($scope.newsArray);
 
 
             });
+            return res;
         };
         $scope.aggregateTopCrimesInCity = function(city){
             var res = Search.aggregateTopCrimesInCity(city).then(function (data) {
-                console.log(data);
-                $scope.newsArray = data.data.aggregations.crimes_count.buckets;
-                console.log("Array", $scope.newsArray);
-                // setMarkersNews($scope.newsArray);
-
-
+                var res1 = data.hits.total;
+                console.log("aggregateTopCrimesInCity res", res1);
+                return res1;
             });
+            return res;
         };
 
 
         $scope.init = function () {
             $scope.aggregateTotalCrimesInCity("Bari");
+           // $scope.aggregateTotalNewsInCity("Bari");
+            //$scope.aggregateTopCrimesInCity("Bari");
         };
 
   }]);
