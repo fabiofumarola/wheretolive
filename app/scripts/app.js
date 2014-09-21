c96ba44534b04dd6f1836faf34c9d961d7d495cd
Giuseppe Piccolo <giuseppe.piccolo1@gmail.com>
2014-09-21 11:57:53 +0200

    add angularCharts dependency

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'google-maps'
+    'google-maps',
+    'angularCharts'
   ])
   .config(function ($routeProvider) {
     $routeProvider
