c8be7e956402f1e7aac79e716dc7c536b6d9b52b
fabiofumarola <fabiofumarola@gmail.com>
2014-09-21 14:31:39 +0200

    review page crimap

--- app/scripts/controllers/crimap.js
+++ app/scripts/controllers/crimap.js
@@ -8,16 +8,8 @@
  * Controller of the wheretoliveApp
  */
 angular.module('wheretoliveApp')
-    .controller('CrimapCtrl', ['$scope', 'Search',
-        function ($scope, Search){
-            /*
-             Posizione di default  Palazzo San Gervaso
-             */
-            console.log(Search);
-            $scope.city=undefined;
-
-
-
+    .controller('CrimapCtrl', ['$scope', 'Search','$http',
+        function ($scope, Search, $http){
             $scope.map = {
                 center: {
                     latitude: '41',
@@ -29,17 +21,34 @@ angular.module('wheretoliveApp')
             $scope.options = {
                 value: 10
             };
+
             $scope.searchCrimes = function () {
                 //console.log($scope.city);
-                console.log($scope.search.result);
-                console.log($scope.search.options);
+                console.log($scope.search);
+                //console.log($scope.search.options);
             };
 
+            $scope.getCrimesArray = function(){
+                //do get from data/listaReati.json
+                $http({method: 'GET', url: '/data/listaReati.json'}).
+                    success(function(data, status, headers, config) {
+                        $scope.crimesList = data.slice(0,20);
+                        console.log(JSON.stringify(data.slice(0,20)));
+                    }).
+                    error(function(data, status, headers, config) {
+                        console.log("error");
+                    });
+            };
 
+            $scope.getCrimeMarkers = function(city, filter){
 
+            };
 
-
-
-
-
+            /*
+             Init è una funzione speciale che viene richiamata ad ogni refresh della pagina.
+             Chiamata in news.html
+             */
+            $scope.init = function () {
+                $scope.getCrimesArray();
+            };
         }]);
